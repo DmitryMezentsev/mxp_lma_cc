e9ca53802fe242ae50ca5e617721f09a386d8bcc
Sergey Voldaitsev <sv@host>
2019-04-05 22:52:31 +0300

    add env.js

---
+++ src/common/js/env.js
@@ -0,0 +1,20 @@
+// Путь к API
+export const API_PATH = 'http://api.lma.mxpdev.com/';
+
+// Путь к бэкенду (не API)
+export const OTH_SERVER_PATH = 'http://oth-lma.mxpdev.com/';
+
+// Путь к серверу авторизации
+export const AUTH_SERVER_PATH = 'http://sso.dev.maxitechcore.ru/';
+
+// Client ID для SSO-авторизации
+export const CLIENT_ID = 'ce95a469-61bf-4935-9388-490795aba85d';
+
+// Google Maps API key
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyBOtcyrSzcub7xVBbgho3Gyit6QlHgpP_I';
+// Кординаты начального центра карты
+export const MAP_CENTER = {
+    lat: 55.751244,
+    lng: 37.618423,
+};
+
