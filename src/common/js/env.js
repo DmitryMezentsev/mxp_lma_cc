45a4442e3735e7e48c4260c5349bfca6006b2b98
Vladislav Cherukhin <vladislav.cherukhin@mxpdev.com>
2019-05-29 07:40:04 +0000

    Update env.js

--- src/common/js/env.js
+++ src/common/js/env.js
@@ -5,10 +5,10 @@ export const API_PATH = 'http://api.lma.mxpdev.com/';
 export const OTH_SERVER_PATH = 'http://oth-lma.mxpdev.com/';
 
 // Путь к серверу авторизации
-export const AUTH_SERVER_PATH = 'http://sso.dev.maxitechcore.ru/';
+export const AUTH_SERVER_PATH = 'https://sso.mxp.dev/';
 
 // Client ID для SSO-авторизации
-export const CLIENT_ID = 'ce95a469-61bf-4935-9388-490795aba85d';
+export const CLIENT_ID = 'ce95a469-61bf-4935-9388-490795aba85f';
 
 // Google Maps API key
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBOtcyrSzcub7xVBbgho3Gyit6QlHgpP_I';
