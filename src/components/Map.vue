c84ab3a97f2d6c31561a8120b8ea02c0c4882a9c
Dmitry <mezentsevdmitry51@gmail.com>
2019-07-24 20:24:02 +0500

    -

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <GmapMap ref="map" :center="MAP_CENTER" :zoom="zoom" :options="mapOptions" :style="{ height }">
-      <GmapCluster>
+      <GmapCluster :minimum-cluster-size="5" zoom-on-click>
         <GmapMarker
           v-for="(marker, i) in markers"
           :key="i"
           :title="marker.title"
-          :icon="getMarkerIcon(i)"
+          :icon="marker.color === 'red' ? markerIconRed : markerIconGreen"
           :position="{ lat: marker.lat, lng: marker.lng }"
           clickable
-          @mouseover="markerHover(i)"
-          @mouseout="markerHover(i, true)"
           @click="markerClick(i)"
         />
       </GmapCluster>
@@ -24,9 +22,7 @@ import GmapCluster from 'vue2-google-maps/dist/components/cluster';
 
 import { MAP_CENTER } from 'Common/js/env';
 import markerIconGreen from 'Common/img/gmap-marker-green.png';
-import markerIconHoverGreen from 'Common/img/gmap-marker-hover-green.png';
 import markerIconRed from 'Common/img/gmap-marker-red.png';
-import markerIconHoverRed from 'Common/img/gmap-marker-hover-red.png';
 
 export default {
   name: 'Map',
@@ -61,43 +57,19 @@ export default {
           },
         ],
       },
+      markerIconGreen,
+      markerIconRed,
     };
   },
   methods: {
     markerClick(i) {
       this.$emit('marker-click', i);
     },
-    markerHover(i, out = false) {
-      this.$set(this.markersColors, i, {
-        ...this.markersColors[i],
-        hover: !out,
-      });
-    },
-    getMarkerIcon(i) {
-      if (this.markersColors[i].color === 'red') {
-        return this.markersColors[i].hover ? markerIconHoverRed : markerIconRed;
-      }
-
-      return this.markersColors[i].hover ? markerIconHoverGreen : markerIconGreen;
-    },
   },
   mounted() {
     this.$refs.map.$mapPromise.then(map => {
       this.$emit('init', map);
     });
-  },
-  watch: {
-    markers: {
-      handler(markers) {
-        this.markersColors = markers.map(m => {
-          return {
-            color: m.color || 'green',
-            hover: false,
-          };
-        });
-      },
-      immediate: true,
-    },
   },
 };
 </script>
